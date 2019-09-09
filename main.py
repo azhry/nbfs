96a50d79e4b598c90016d7b0c8e669876b9d1a5d
azhry <arliansyah_azhary@yahoo.com>
2019-09-09 23:56:52 +0700

    commented designer command

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # source .venv/Scripts/activate
+# .venv/Lib/site-packages/pyqt5_tools/Qt/bin/designer.exe --> start designer
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_excel('data/nbfs.xlsx', header=0)
+print(dataset.head(5))
